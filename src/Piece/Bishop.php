77c9ee6ac6055074b1010217003c589ab5b7fdf2
tokyandria001 <toky.andrianjafy2001@gmail.com>
2026-03-05 18:19:01 +0100

    Bishop

---
+++ src/Piece/Bishop.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Chess\Piece;
+
+use Chess\Position;
+use Chess\Enum\PieceColor;
+use Chess\Enum\PieceType;
+
+class Bishop extends Piece
+{
+    public function __construct(PieceColor $color, Position $position)
+    {
+        parent::__construct($color, $position);
+        $this->type = PieceType::BISHOP;
+    }
+
+    protected function isValidMovementShape(Position $target): bool
+    {
+        $fromRow = $this->position->getRow();
+        $fromCol = $this->position->getColumn();
+        $toRow = $target->getRow();
+        $toCol = $target->getColumn();
+
+        $rowDistance = abs($toRow - $fromRow);
+        $colDistance = abs($toCol - $fromCol);
+
+        return $rowDistance === $colDistance && $rowDistance > 0;
+    }
+}
